61760c441e5f45fb4feba811a53d7bd84e0bf157
Tom Bouttaz <thomas_bouttaz@dell.com>
2025-07-17 11:25:47 +0100

    Lift isBackstagePasses

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -16,6 +16,7 @@ public class Item {
 
     void updateItem() {
         boolean isAgedBrie = name.equals("Aged Brie");
+        boolean isBackstagePasses = name.equals("Backstage passes to a TAFKAL80ETC concert");
 
         if (isAgedBrie) {
             if (quality < 50) {
@@ -31,13 +32,7 @@ public class Item {
                 }
             }
         } else {
-            if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (quality > 0) {
-                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                        quality = quality - 1;
-                    }
-                }
-            } else {
+            if (isBackstagePasses) {
                 if (quality < 50) {
                     quality = quality + 1;
 
@@ -53,21 +48,29 @@ public class Item {
                         }
                     }
                 }
-            }
 
-            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
                 sellIn = sellIn - 1;
-            }
 
-            if (sellIn < 0) {
-                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+            } else {
+                if (quality > 0) {
+                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                        quality = quality - 1;
+                    }
+                }
+
+                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                    sellIn = sellIn - 1;
+                }
+
+                if (sellIn < 0) {
                     if (quality > 0) {
                         if (!name.equals("Sulfuras, Hand of Ragnaros")) {
                             quality = quality - 1;
                         }
                     }
-                } else {
-                    quality = 0;
                 }
             }
         }
