695fefec70930957b3a6b6ae29fc439f7764ccfb
Tom Bouttaz <thomas_bouttaz@dell.com>
2025-07-17 11:36:32 +0100

    Merge ifs

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -21,51 +21,42 @@ public class Item {
 
         if (isAgedBrie) {
             if (quality < 50) {
-                quality = quality + 1;
-
+                quality++;
             }
 
             sellIn = sellIn - 1;
 
-            if (sellIn < 0) {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
+            if (sellIn < 0 && quality < 50) {
+                quality++;
             }
         } else {
             if (isBackstagePasses) {
                 if (quality < 50) {
-                    quality = quality + 1;
+                    quality++;
 
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
+                    if (sellIn < 11 && quality < 50) {
+                        quality++;
                     }
 
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
+                    if (sellIn < 6 && quality < 50) {
+                        quality++;
                     }
                 }
 
-                sellIn = sellIn - 1;
+                sellIn--;
 
                 if (sellIn < 0) {
                     quality = 0;
                 }
             } else if (!isSulfuras) {
                 if (quality > 0) {
-                    quality = quality - 1;
+                    quality--;
                 }
 
-                sellIn = sellIn - 1;
+                sellIn--;
 
-                if (sellIn < 0) {
-                    if (quality > 0) {
-                        quality = quality - 1;
-                    }
+                if (sellIn < 0 && quality > 0) {
+                    quality--;
                 }
             }
         }
